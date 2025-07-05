7466004a91d7d51762cba2e65dca350236693c86
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-07-05 14:24:16 -0300

    chore: adicionando o projeto spring

---
+++ src/main/java/br/peerplay/PeerplayApplication.java
@@ -0,0 +1,13 @@
+package br.peerplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/peerplay/PeerplayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.peerplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
