6785d215328c35242e13b701c712783b632569d3
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-07-06 23:03:34 -0300

    feat: construindo comment enttity

---
+++ src/main/java/br/peerplay/domain/comment/Comment.java
@@ -0,0 +1,61 @@
+package br.peerplay.domain.comment;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import br.peerplay.domain.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User comentator;
+
+    @ManyToOne
+    @JoinColumn(name = "video_id")
+    private Video video;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_comment_id")
+    @JsonBackReference
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment")
+    @JsonManagedReference
+    private List<Comment> children;
+
+    @Column(name = "timestamp_seconds", nullable = false)
+    private int timestampSeconds;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+}
